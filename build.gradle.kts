98639dc978bbc8d7c2c7c4e97b586464ad2b8c1c
Michael Matzka <14311597+mima0815@users.noreply.github.com>
2025-09-24 10:30:13 +0200

    fix(jreleaser): increase timeouts before jrelease fails

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,17 +27,18 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.9")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.17")
 
     testImplementation("org.mockito:mockito-inline:4.11.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.3")
 
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    testCompileOnly("org.projectlombok:lombok:1.18.30")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+    testCompileOnly("org.projectlombok:lombok:1.18.42")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
 }
 
 tasks.test {
@@ -142,6 +143,8 @@ configure<org.jreleaser.gradle.plugin.JReleaserExtension> {
                     active.set(org.jreleaser.model.Active.RELEASE)
                     url = "https://central.sonatype.com/api/v1/publisher"
                     stagingRepository(releasesRepoUrl.get().asFile.path)
+                    retryDelay = 30
+                    maxRetries = 100
                 }
             }
             nexus2 {
