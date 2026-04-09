af59bfd5e2a3d50a49398c9006d62ce5b0816973
Akanksha Acharya <akanksha4510471@gmail.com>
2026-04-09 10:17:30 +0530

    test changes

---
+++ src/main/java/com/factset/sdk/eventdriven/factsettrading/model/tradeevents/repeatinggroup/GroupEntry.java
@@ -0,0 +1,10 @@
+﻿package com.factset.sdk.eventdriven.factsettrading.model.tradeevents.repeatinggroup;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GroupEntry{
+    List<TagValuePair> value;
+}
---
+++ src/main/java/com/factset/sdk/eventdriven/factsettrading/model/tradeevents/repeatinggroup/MessageValue.java
@@ -0,0 +1,10 @@
+﻿package com.factset.sdk.eventdriven.factsettrading.model.tradeevents.repeatinggroup;
+
+import lombok.Data;
+
+@Data
+public class MessageValue{
+    String stringValue;
+    
+    RepeatingGroup repeatingGroupValue;;
+}
